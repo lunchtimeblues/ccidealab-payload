6c206b78898db561d7894169182c15eb7b181d61
lunchtimeblues <dhsieh151@gmail.com>
2025-08-05 23:56:44 +0000

    SmoothScroll Fix

--- src/components/SmoothScroll/index.tsx
+++ src/components/SmoothScroll/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from '@studio-freight/lenis' // You're still using the old package
 
 interface SmoothScrollProps {
   children: React.ReactNode
@@ -9,28 +9,21 @@ interface SmoothScrollProps {
 
 export const SmoothScroll = ({ children }: SmoothScrollProps) => {
   useEffect(() => {
-    // Initialize Lenis
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      direction: 'vertical', // vertical, horizontal
-      gestureDirection: 'vertical', // vertical, horizontal, both
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       touchMultiplier: 2,
       infinite: false,
     })
 
-    // Get scroll value and dispatch custom event for other components
     lenis.on('scroll', (e: any) => {
-      // Dispatch custom event for parallax components
-      window.dispatchEvent(new CustomEvent('lenis-scroll', {
-        detail: { scroll: e.scroll, velocity: e.velocity }
-      }))
+      window.dispatchEvent(
+        new CustomEvent('lenis-scroll', {
+          detail: { scroll: e.scroll, velocity: e.velocity },
+        }),
+      )
     })
 
-    // Use requestAnimationFrame to continuously update the scroll
     function raf(time: number) {
       lenis.raf(time)
       requestAnimationFrame(raf)
@@ -38,7 +31,6 @@ export const SmoothScroll = ({ children }: SmoothScrollProps) => {
 
     requestAnimationFrame(raf)
 
-    // Cleanup
     return () => {
       lenis.destroy()
     }
